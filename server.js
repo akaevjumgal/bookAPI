9da40e585474d054bcf1dd3d11fd28c92870a40e
jumgal <akaev.jumgal@gmail.com>
2018-06-01 12:08:31 +0600

    Added new single pages for each card

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 5000
+
+// var id = require('./public/index.html')
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render("index")
+})
+
+app.listen(port, () => console.log("Server is serving in port " + port))
+
+app.get('/post/:id', (req, res) => {
+  res.render('post', { post_id: req.params.id })
+})
